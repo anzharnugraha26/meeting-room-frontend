7dbc52fb1203f03c87a706908beb21c36f7f5dcc
anzharnugraha26 <anzharnugraha50@gmail.com>
2026-01-28 13:23:49 +0700

    UP

--- src/app/api/bookings/[id]/status/route.ts
+++ src/app/api/bookings/[id]/status/route.ts
@@ -1,30 +1,61 @@
 import { NextRequest, NextResponse } from "next/server"
 import { BASE_URL } from "@/src/lib/api"
 
+// export async function PATCH(
+//   req: NextRequest,
+//   context: { params: Promise<{ id: string }> }
+// ) {
+//   try {
+//     console.log("masuk route")
+
+//     const { id: bookingId } = await context.params
+//     const { status } = await req.json()
+
+//     const backendRes = await fetch(
+//       `${BASE_URL}/bookings/${bookingId}/status`,
+//       {
+//         method: "PATCH",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ status }),
+//         cache: "no-store",
+//       }
+//     )
+
+//     const data = await backendRes.json()
+
+//     return NextResponse.json(data, { status: backendRes.status })
+//   } catch (error) {
+//     console.error("PROXY UPDATE STATUS ERROR:", error)
+//     return NextResponse.json(
+//       { message: "Failed to connect to backend service" },
+//       { status: 500 }
+//     )
+//   }
+// }
+
+// import { NextRequest, NextResponse } from "next/server"
+
 export async function PATCH(
   req: NextRequest,
-  context: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
   try {
-    console.log("masuk route")
-
-    const { id: bookingId } = await context.params
+    const bookingId = params.id
     const { status } = await req.json()
 
     const backendRes = await fetch(
       `${BASE_URL}/bookings/${bookingId}/status`,
       {
         method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ status }),
         cache: "no-store",
       }
     )
 
     const data = await backendRes.json()
-
     return NextResponse.json(data, { status: backendRes.status })
   } catch (error) {
     console.error("PROXY UPDATE STATUS ERROR:", error)

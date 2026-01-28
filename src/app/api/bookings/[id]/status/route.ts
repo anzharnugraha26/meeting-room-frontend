b8fa5986935717c678ca34a5bc8dc7000a5c4385
anzharnugraha26 <anzharnugraha50@gmail.com>
2026-01-28 13:38:54 +0700

    up 1

--- src/app/api/bookings/[id]/status/route.ts
+++ src/app/api/bookings/[id]/status/route.ts
@@ -1,48 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 import { BASE_URL } from "@/src/lib/api"
 
-// export async function PATCH(
-//   req: NextRequest,
-//   context: { params: Promise<{ id: string }> }
-// ) {
-//   try {
-//     console.log("masuk route")
-
-//     const { id: bookingId } = await context.params
-//     const { status } = await req.json()
-
-//     const backendRes = await fetch(
-//       `${BASE_URL}/bookings/${bookingId}/status`,
-//       {
-//         method: "PATCH",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ status }),
-//         cache: "no-store",
-//       }
-//     )
-
-//     const data = await backendRes.json()
-
-//     return NextResponse.json(data, { status: backendRes.status })
-//   } catch (error) {
-//     console.error("PROXY UPDATE STATUS ERROR:", error)
-//     return NextResponse.json(
-//       { message: "Failed to connect to backend service" },
-//       { status: 500 }
-//     )
-//   }
-// }
-
-// import { NextRequest, NextResponse } from "next/server"
-
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const bookingId = params.id
+    const { id: bookingId } = await params   // ⬅️ WAJIB await
+
     const { status } = await req.json()
 
     const backendRes = await fetch(
